9a1fa0c770938ef583ab5c3ab82883c115afc928
KatsCyl <ahti.katiska@relex.fi>
2016-11-26 23:11:44 +0200

    fixed

--- src/enemy.ts
+++ src/enemy.ts
@@ -35,4 +35,37 @@ module Game {
             SOLDIER
         }
     }
+
+export class Civilian extends Enemy {
+        public static readonly SPRITE_KEY = "civilian";
+
+        private ticker: number = 0
+
+        constructor(game: Phaser.Game, x: number, y: number, group: Phaser.Group) {
+           super(game, x, y, Civilian.SPRITE_KEY, group)
+
+           this.health = 10
+           this.speed = 100
+           this.sprite.body.velocity.x = this.speed
+        }
+
+        move(player: Player): void {
+           if (this.ticker % 60 === 0) {
+              // change direction
+              this.sprite.body.velocity.x *= -1;
+              this.ticker = 0;
+           }
+
+           this.ticker++;
+        }
+
+        attack(player: Player): void {
+           return;
+        }
+
+        getType(): Enemy.Type {
+           return Enemy.Type.CIVILIAN
+        }
+    }
+
 }
