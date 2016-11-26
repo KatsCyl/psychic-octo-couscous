4d66c5bc3d9752643292e16cddc88b8c33e40f43
KatsCyl <ahti.katiska@relex.fi>
2016-11-26 19:58:41 +0200

    SHUNSAHNSTUHSAEUTNAE

--- src/playState.ts
+++ src/playState.ts
@@ -5,7 +5,15 @@ module Game {
         private floorGroup: Phaser.Group
         private obstacleGroup: Phaser.Group
 
+        // collision group
+        private collisionGroup: Phaser.Group
+        
+        // change backgroud to scrolling parallax
         private background: Phaser.Sprite
+        private floor: Phaser.Sprite
+
+        // Foregroud wall
+        private foreGround: Phaser.Physics.Arcade.Body
 
         private player: Player
 
@@ -14,19 +22,32 @@ module Game {
         }
 
         create () {
-            this.floorGroup = new Phaser.Group(this.game);
-            this.obstacleGroup = new Phaser.Group(this.game);
+            this.floorGroup = new Phaser.Group(this.game)
+            this.obstacleGroup = new Phaser.Group(this.game)
+            this.collisionGroup = new Phaser.Group(this.game)
 
             this.background = this.game.add.sprite(0, 0, 'general', this.floorGroup)
-            this.background.width = this.game.camera.width
-            this.background.height = this.game.camera.height
+            this.background.width = this.game.camera.width * 300
+            this.background.height = this.game.camera.height * 1/3
+            this.game.physics.arcade.enableBody(this.background)
+            this.background.body.immovable = true
 
-            this.player = new Player(this.game, 100, 100, 'playerAnimations', this.obstacleGroup) 
+            this.collisionGroup.add(this.background)
+
+            this.player = new Player(this.game, 100, 300, 'playerAnimations', this.obstacleGroup) 
+
+            this.collisionGroup.add(this.player.sprite)
 
         }
 
         update () {
-            this.player.update();
+            this.player.update()
+            this.physics.arcade.collide(this.collisionGroup)
+        }
+
+        render () {
+            this.game.debug.body(this.player.sprite)
+            this.game.debug.spriteBounds(this.player.sprite)
         }
     }
 }
--- src/player.ts
+++ src/player.ts
@@ -2,19 +2,25 @@ module Game {
     export class Player {
 
         private cursors: Phaser.CursorKeys
-        private sprite: Phaser.Sprite
+        public sprite: Phaser.Sprite
         private flipped: boolean = false
 
         constructor(game: Phaser.Game, x: number, y: number, animations: any, group: Phaser.Group) {
             this.sprite = game.add.sprite(x, y, animations, group)
             this.sprite.anchor = new Phaser.Point(0.5, 0.5)
             this.sprite.smoothed = false;
-            this.sprite.scale = new Phaser.Point(2.5, 2.5)
 
             this.sprite.animations.add('idle', [0, 1, 2, 3], 5, true)
             this.sprite.animations.add('move', [4, 5, 6, 7, 8], 5, true)
             this.sprite.animations.play('idle')
+
             game.physics.arcade.enableBody(this.sprite)
+
+            this.sprite.body.checkCollision = true
+            this.sprite.body.setSize(this.sprite.width * 0.25, this.sprite.height * 0.1, this.sprite.width * (0.5 - 0.25/2), this.sprite.height * 0.9)
+
+            this.sprite.scale = new Phaser.Point(1.5, 1.5)
+
             this.cursors = game.input.keyboard.createCursorKeys();
         }
 
@@ -41,6 +47,12 @@ module Game {
                 }
                 this.sprite.animations.play('move')
                 this.sprite.body.velocity.x = 15
+            } else if ( this.cursors.up.isDown) {
+                this.sprite.animations.play('move')
+                this.sprite.body.velocity.y = -15
+            } else if ( this.cursors.down.isDown) {
+                this.sprite.animations.play('move')
+                this.sprite.body.velocity.y = 15
             } else {
                 this.sprite.animations.play('idle')
                 this.sprite.body.velocity.x = 0
