2adab7499eecb25f883587eeeed020d9c7af4f58
KatsCyl <ahti.katiska@relex.fi>
2016-11-26 16:47:41 +0200

    asd

--- src/playState.ts
+++ src/playState.ts
@@ -1,19 +1,21 @@
 module Game {
     export class playState extends Phaser.State {
 
-        public iso: Phaser.Plugin.Isometric
+        // Isometric plugin
+        private iso: Phaser.Plugin.Isometric
 
+        // Sprite groups
         private floorGroup: Phaser.Group
         private obstacleGroup: Phaser.Group
 
-        private sprite: Phaser.Sprite
+        private background: Phaser.Sprite
 
         preload () {
-            this.game.plugins.add(Phaser.Plugin.Isometric)
             this.iso = new Phaser.Plugin.Isometric(this.game)
+            this.game.plugins.add(Phaser.Plugin.Isometric)
             this.game.world.setBounds(0, 0, 2048, 1024)
             this.game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE)
-            this.iso.projector.anchor = new Phaser.Point(0.5, 0.5)
+            this.iso.projector.anchor = new Phaser.Point(0, 0)
 
         }
 
@@ -21,7 +23,10 @@ module Game {
             this.floorGroup = new Phaser.Group(this.game);
             this.obstacleGroup = new Phaser.Group(this.game);
 
-            this.sprite = this.iso.addIsoSprite(500, 500, 0, 'general', 0, this.floorGroup);
+            this.background = this.iso.addIsoSprite(0, 0, 0, 'general', this.floorGroup)
+            this.background.width = this.game.camera.width
+            this.background.height = this.game.camera.height
+
         }
     }
 }
